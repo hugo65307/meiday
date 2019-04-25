7268628f19e76f54c13f022ab90f29f939880f89
aluno <aluno@aluno.com>
2019-04-25 11:47:50 -0300

    Alteracao no login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,16 @@
 <template>
   <v-layout align-center justify-center row fill-height>
     <v-flex xs12 sm3 class="ma-2">
-      <v-card class="card-login" dark color="light-blue accent-3">
-        <v-toolbar color="cyan accent-2">
-          <v-toolbar-title>Faça seu Login!</v-toolbar-title>
+      <v-card class="card-login pb-3" width="400" color="cinza escurecer-3">
+        <v-toolbar id="facalogin">
+          <v-toolbar-title id="escritafacaseulogin">Faça seu Login!</v-toolbar-title>
         </v-toolbar>
         <form class="pa-3" >
-          <v-text-field label="E-mail"></v-text-field>
-          <v-text-field label="Senha"></v-text-field>
+          <v-text-field label="E-mail" outline></v-text-field>
+          <v-text-field label="Senha" outline></v-text-field>
         </form>
         <v-card-actions>
-          <v-btn id="pedro" color="green lighten-5" block round>Logar</v-btn>
+          <v-btn id="pedro" block round>Logar</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -20,19 +20,26 @@
  
 </script>
 <style scoped>
-#pedro{
-  color: black;
-  font-weight: bold;
-  font-family: GungsuhChe;
-  font-size: 150%;
-}
-  .layout{
+.layout{
     background-image: url('../assets/fundo.jpg');
     background-size: cover;
     background-position: center;
   }
-  .card-login{
-    width: 600px;
-    right: 100px;
+#pedro{
+  background-color:  #00C853;
+  color: black;
+  font-weight: bold;
+  font-family: arial;
+  font-size: 100%;
+}
+  #facalogin{
+    background-color:  #00C853;
+
   }
+  #escritafacaseulogin{
+    color: #000000;
+  }
+
+
+
 </style>
