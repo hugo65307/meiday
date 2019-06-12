57fc710af5f58be8216fab68b009f4f73b396edb
Aluno <aluno@aluno.com>
2019-06-12 14:27:46 -0300

    loginprincipal

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,16 @@
 <template>
   <v-layout align-center justify-center row fill-height>
     <v-flex xs12 sm3 class="ma-2">
-      <v-card class="card-login" dark color="light-blue accent-3">
-        <v-toolbar color="cyan accent-2">
-          <v-toolbar-title>Faça seu Login!</v-toolbar-title>
+      <v-card class="card-login" dark color="blue-grey darken-4">
+        <v-toolbar color="blue-grey darken-3">
+        <v-toolbar-title>Faça seu Login!</v-toolbar-title>
         </v-toolbar>
         <form class="pa-3" >
           <v-text-field label="E-mail"></v-text-field>
           <v-text-field label="Senha"></v-text-field>
         </form>
         <v-card-actions>
-          <v-btn id="pedro" color="green lighten-5" block round>Logar</v-btn>
+          <v-btn id="pedro" color="green lighten-5" block round>logar</v-btn>
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -21,9 +21,9 @@
 </script>
 <style scoped>
 #pedro{
-  color: black;
+  color: rgb(3, 3, 3);
   font-weight: bold;
-  font-family: GungsuhChe;
+  font-family: Times New Roman;
   font-size: 150%;
 }
   .layout{
@@ -32,7 +32,7 @@
     background-position: center;
   }
   .card-login{
-    width: 600px;
+    width: 400px;
     right: 100px;
   }
 </style>
